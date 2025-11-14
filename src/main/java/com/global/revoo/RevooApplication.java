b142b8d6203ded1c387edec5daef25b7822a674c
VoyDcode <Victor_r.lima@hotmail.com>
2025-11-14 00:59:53 -0300

    Iniciando o projeto.

---
+++ src/main/java/com/global/revoo/RevooApplication.java
@@ -0,0 +1,14 @@
+package com.global.revoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevooApplication.class, args);
+	}
+	
+
+}
---
+++ src/test/java/com/global/revoo/RevooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global.revoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
